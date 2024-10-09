e4043377eb8bd2a24f59ad7d89d0e8483b7b2ab8
gushengbing <17612168142@163.com>
2024-10-09 16:15:46 +0800

    11

--- src/main/java/com/bmg/mall/BmgApplication.java
+++ src/main/java/com/bmg/mall/BmgApplication.java
@@ -9,5 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BmgApplication {
     public static void main(String[] args) {
         SpringApplication.run(BmgApplication.class,args);
+
+
     }
 }
